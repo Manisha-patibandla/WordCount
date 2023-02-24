4a70f2b644a49aff33f84831c49b9bff2e807529
Manisha-patibandla <manishapatibandla1@gmail.com>
2023-02-24 22:08:13 +0530

    first commit

---
+++ Output1.java
@@ -0,0 +1,4 @@
+is:1
+every:1
+day:2
+good:1
---
+++ WordCount.java
@@ -0,0 +1,29 @@
+import java.util.*;
+//import java.util.Map.Entry;
+public class WordCount
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+    
+        HashMap<String,Integer> map=new HashMap<>();
+        String str="every day is good day";
+        String word[]=str.split(" ");
+        
+        
+    for(String  w:word)
+    {
+        if(map.containsKey(w)){
+            map.put(w,map.get(w)+1);
+        }
+        else
+        {
+            map.put(w,1);
+        }
+    }
+    for(Map.Entry<String,Integer> ele:map.entrySet())
+    {
+        System.out.println(ele.getKey()+":"+ele.getValue());
+    }
+    }
+}
